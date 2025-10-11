2e8805af7e16bec6c8474dd7bce871f4e793d3e9
hrncwzam <kanna_nd@hotmail.co.jp>
2025-10-11 16:34:02 +0900

    events.json呼び出し時のキャッシュバスター追加(10分単位)

--- events.js
+++ events.js
@@ -1,5 +1,14 @@
+function getCacheBusterKey() {
+  const now = new Date();
+  const minutes = now.getUTCMinutes();
+  const rounded = Math.floor(minutes / 10) * 10; // 10分単位に丸める
+  const key = `${now.getUTCFullYear()}${now.getUTCMonth() + 1}${now.getUTCDate()}${now.getUTCHours()}${rounded}`;
+  return key;
+}
+
 async function loadEvents() {
-    const response = await fetch('./events.json');
+    const cacheKey = getCacheBusterKey();
+    const response = await fetch('./events.json?cd=${cacheKey}');
     if(!response.ok) {
         console.error('Failed to load events.json',response.status);
         return [];
